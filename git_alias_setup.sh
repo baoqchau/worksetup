2b862a800268f65a75b6d1d725a30ad108aff266
Quan Chau <quan.chau@appfolio.com>
2016-09-17 23:30:14 -0700

    add script to setup git aliases

---
+++ git_alias_setup.sh
@@ -0,0 +1,9 @@
+git config --global alias.lg 'log --graph --decorate'
+git config --global alias.lgo 'log --graph --decorate --oneline'
+git config --global alias.ap 'add -p'
+git config --global alias.ph 'push origin HEAD'
+git config --global alias.cia 'commit --amend'
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
