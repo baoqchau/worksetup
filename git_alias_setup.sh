a23951092950c6da9126a4e3c2a9a6330b1b145c
Quan Chau <quan.chau@appfolio.com>
2016-10-25 08:50:21 -0700

    update git alias

--- git_alias_setup.sh
+++ git_alias_setup.sh
@@ -7,3 +7,13 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.cp cherry-pick
+git config --global alias.rbi 'rebase -i'
+git config --global alias.rbc 'rebase --continue'
+git config --global alias.lgv 'log --decorate --reverse'
+git config --global alias.H HEAD
+git config --global alias.rh 'reset HEAD~1'
+git config --global alias.cpc 'cherry-pick --continue'
+git config --global alias.cif 'commit --fixup'
+git config --global alias.rbia 'rebase -i --autosquash'
+git config --global alias.ss 'show --stat'
